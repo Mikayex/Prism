df910fc18f8aff73d29a87b8235082437079e086
Thomas Laroche <tho.laroche@gmail.com>
2023-07-20 21:16:29 +0200

    Try to fix clang compilation

--- src/VulkanUtils.hpp
+++ src/VulkanUtils.hpp
@@ -5,13 +5,13 @@
 #include <vector>
 #include <vulkan/vulkan.hpp>
 
-template<typename T, typename Dispatch, std::enable_if_t<vk::isVulkanHandleType<T>::value, bool> = true>
-bool operator==(vk::UniqueHandle<T, Dispatch>& handle, std::nullptr_t nullptrValue) {
+template<typename T, typename Dispatch>
+bool operator==(vk::UniqueHandle<T, Dispatch>& handle, std::nullptr_t) {
   return !handle;
 }
 
 template<typename T, typename = std::enable_if_t<vk::isVulkanHandleType<T>::value>>
-bool operator==(T& handle, std::nullptr_t nullptrValue) {
+bool operator==(T& handle, std::nullptr_t) {
   return !handle;
 }
 

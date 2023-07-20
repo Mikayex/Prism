21c3e217d5d5f2a9de96e2e1f8bc7ab25d261550
Thomas Laroche <tho.laroche@gmail.com>
2023-07-20 21:21:50 +0200

    Try to fix clang compilation

--- src/VulkanUtils.hpp
+++ src/VulkanUtils.hpp
@@ -5,15 +5,17 @@
 #include <vector>
 #include <vulkan/vulkan.hpp>
 
+namespace vk {
 template<typename T, typename Dispatch>
-bool operator==(vk::UniqueHandle<T, Dispatch>& handle, std::nullptr_t) {
+bool operator==(const UniqueHandle<T, Dispatch>& handle, std::nullptr_t) {
   return !handle;
 }
 
-template<typename T, typename = std::enable_if_t<vk::isVulkanHandleType<T>::value>>
-bool operator==(T& handle, std::nullptr_t) {
+template<typename T, typename = std::enable_if_t<isVulkanHandleType<T>::value>>
+bool operator==(const T& handle, std::nullptr_t) {
   return !handle;
 }
+}  // namespace vk
 
 namespace Prism {
 std::vector<const char*> toCharArrays(const std::unordered_set<std::string>& strings);
